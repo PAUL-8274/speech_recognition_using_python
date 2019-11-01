6c5912a991601f93158e36289c2bc81c2f114f1c
PAULAMI DAS <52161411+PAUL-8274@users.noreply.github.com>
2019-11-01 22:50:20 +0530

    Delete microphone.py

--- microphone.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-r = sr.Recognizer()
-mic = sr.Microphone()
-sr.Microphone.list_microphone_names()
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-print(r.recognize_google(audio))
